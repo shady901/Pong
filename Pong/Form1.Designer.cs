500ce9d5f0e81e640b6dffae1870763cf4f8264e
shady901 <shady90111@gmail.com>
2019-01-08 10:53:15 +1300

    commiting

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pong
 {
-    partial class Form1
+    partial class Frm_Pong
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pb_player1 = new System.Windows.Forms.PictureBox();
+            this.pb_player2 = new System.Windows.Forms.PictureBox();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_player1
+            // 
+            this.pb_player1.BackColor = System.Drawing.SystemColors.Control;
+            this.pb_player1.Location = new System.Drawing.Point(12, 227);
+            this.pb_player1.Name = "pb_player1";
+            this.pb_player1.Size = new System.Drawing.Size(15, 180);
+            this.pb_player1.TabIndex = 0;
+            this.pb_player1.TabStop = false;
+            // 
+            // pb_player2
+            // 
+            this.pb_player2.BackColor = System.Drawing.SystemColors.Control;
+            this.pb_player2.Location = new System.Drawing.Point(757, 227);
+            this.pb_player2.Name = "pb_player2";
+            this.pb_player2.Size = new System.Drawing.Size(15, 180);
+            this.pb_player2.TabIndex = 1;
+            this.pb_player2.TabStop = false;
+            // 
+            // Update
+            // 
+            this.Update.Enabled = true;
+            this.Update.Interval = 50;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // Frm_Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.pb_player2);
+            this.Controls.Add(this.pb_player1);
+            this.MaximizeBox = false;
+            this.Name = "Frm_Pong";
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Frm_Pong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Frm_Pong_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_player2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_player1;
+        private System.Windows.Forms.PictureBox pb_player2;
+        private new System.Windows.Forms.Timer Update;
     }
 }
 
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -7,14 +7,245 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 
 namespace Pong
 {
-    public partial class Form1 : Form
+    public partial class Frm_Pong : Form
     {
-        public Form1()
+        static float playerMoveSpeed = 5f;
+        static Keys key;
+        Random direction = new Random();
+        static int ballSpeed = 10;
+        int currentLeft;
+         int currentTop;
+       public Rectangle ball =new Rectangle(350,300, 20, 20);
+        bool roundStart = true;
+        public Frm_Pong()
         {
             InitializeComponent();
+            
         }
+
+        private void Frm_Pong_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        //put every updateing method in here
+
+        private void Update_Tick(object sender, EventArgs e)
+        {
+            this.CreateGraphics().Clear(Frm_Pong.ActiveForm.BackColor);
+            PlayerMovement();
+            WallCheck();
+            BallMovement();
+            
+            Draw();
+           
+        }
+
+        public void Draw()
+        {
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillEllipse(myBrush,ball);
+            myBrush.Dispose();
+            formGraphics.Dispose();
+        
+
+
+        }
+
+        private void BallMovement()
+        {
+            //if moving left or right move that direction
+            if (currentLeft==1)
+            {
+                ball.X += ballSpeed; //right
+            }
+            else if(currentLeft == 2)
+            {
+                ball.X -= ballSpeed; //left
+            }
+           
+            //if moving up or down move that direction
+            if (currentTop==1)
+            {
+                ball.Y += ballSpeed; //down
+            }
+            else if (currentTop == 2)
+            {
+                ball.Y -= ballSpeed; //up
+            }
+            else if (currentTop == 3)
+            {
+                ball.Y = ball.Y;
+            }
+
+            //when round start random direction
+            if (roundStart)
+            {
+                if (direction.Next(1, 3) == 1)
+                {
+                    currentLeft = 1;
+                }
+                else
+                {
+                    currentLeft = 2;
+                }
+
+                roundStart = false;
+            }
+          
+                //checks if ball is on the same x axis
+            if ((ball.Location.X<pb_player1.Right))
+            {
+                if (((ball.Location.Y >= pb_player1.Location.Y) && (ball.Location.Y <= pb_player1.Location.Y + pb_player1.Height)))
+                {
+                    BallChangeInDirection();
+                    if (((ball.Location.Y >= pb_player1.Location.Y) && (ball.Location.Y <= pb_player1.Location.Y + 36)))
+                    {
+                        currentTop = 2;
+                    }
+                    else if (((ball.Location.Y <= pb_player1.Location.Y + Height) && (ball.Location.Y >= pb_player1.Location.Y + Height - 36)))
+                    {
+                        currentTop = 1;
+                    }
+                }
+
+            }
+            else if ((ball.Location.X + ball.Width > pb_player2.Left))
+            {
+                if (((ball.Location.Y >= pb_player2.Location.Y) && (ball.Location.Y <= pb_player2.Location.Y + pb_player2.Height)))
+                {
+                    BallChangeInDirection();
+                    if (((ball.Location.Y >= pb_player2.Location.Y) && (ball.Location.Y <= pb_player2.Location.Y + 36)))
+                    {
+                        currentTop = 2;
+                    }
+                    else if (((ball.Location.Y <= pb_player2.Location.Y + Height) && (ball.Location.Y >= pb_player2.Location.Y + Height - 36)))
+                    {
+                        currentTop = 1;
+                    }
+                }
+            }
+                
+               
+
+            //if ball hits top or bottom
+            if ((ball.Location.Y <= 0) || (ball.Location.Y >= 800))
+            {
+                
+                if (currentTop == 1)
+                {
+                    currentTop = 2;
+                }
+                else if (currentTop == 2)
+                {
+                    currentTop = 1;
+                }
+                else
+                {
+                    currentTop = 3;
+                }
+                
+            }
+
+          
+
+
+
+        }
+
+
+        public void BallChangeInDirection()
+        {
+
+            //change x
+            if (currentLeft == 1)
+            {
+                currentLeft = 2;
+            }
+            else
+            {
+                currentLeft = 1;
+            }
+            //change y
+
+            if (currentTop == 1)
+            {
+                currentTop = 2;
+            }
+            else if (currentTop == 2)
+            {
+                currentTop = 1;
+            }
+            else
+            {
+                currentTop = 3;
+            }
+        }
+
+        private void PlayerMovement()
+        {
+            //player1
+            if (Keys.W==key)
+            {
+                
+                pb_player1.Top -= (int)playerMoveSpeed;
+            }
+            if (Keys.S == key)
+            {
+                pb_player1.Top += (int)playerMoveSpeed;
+            }
+
+
+            //player2
+            if (Keys.NumPad8 == key)
+            {
+
+                pb_player2.Top -= (int)playerMoveSpeed;
+            }
+            if (Keys.NumPad5 == key)
+            {
+                pb_player2.Top += (int)playerMoveSpeed;
+            }
+
+
+        }
+
+
+        private void WallCheck()
+        {
+            if (pb_player1.Location.Y>=700-pb_player1.Height)
+            {
+                pb_player1.Top = 700 - pb_player1.Height;
+            }
+            if (pb_player1.Location.Y <= 0 )
+            {
+                pb_player1.Top = 0;
+            }
+            if (pb_player2.Location.Y >= 700 - pb_player2.Height)
+            {
+                pb_player2.Top = 700 - pb_player2.Height;
+            }
+            if (pb_player2.Location.Y <= 0)
+            {
+                pb_player2.Top = 0;
+            }
+        }
+
+
+        private void Frm_Pong_KeyDown(object sender, KeyEventArgs e)
+        {
+          key = e.KeyCode;
+            
+        }
+
+       
     }
 }
